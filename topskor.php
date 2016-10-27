168754151a77d57deb2d19f956d8c452575f41b8
MuhBayu <bnugraha00@hotmail.com>
2016-10-27 14:29:13 +0700

    update APIKey

--- topskor.php
+++ topskor.php
@@ -1,4 +1,9 @@
 <?php
+// ---------------------------------------
+// API Cek Top Skor Liga
+// Get API-KEy? --> http://bayuu.net/api/
+// ---------------------------------------
+
 function customError($errno, $errstr) {
     header('Content-type: application/json');
     $output['status']       = 'Gagal';
@@ -8,12 +13,9 @@ function customError($errno, $errstr) {
 } 
 
 set_error_handler("customError");
-
-
 if(isset($_POST['liga'])) {
-
-$host = 'http://bayuu.net'; // alternatif : bayyu.me / bayyu.cf
-
+$host = 'http://bayuu.net'; 
+$key  = 'YOUR API-KEY HERE'; // Masukan API KEY yang ada punya
 switch ($_POST['liga']) {
 	case 'inggris':
 		$server	=	$host . "/api/cek-topskor?liga=inggris";
@@ -28,25 +30,21 @@ switch ($_POST['liga']) {
 		$server	=	$host . "/api/cek-topskor?liga=champions";
 		break;
 }
-
-  $data = file_get_contents($server);
-
+  $data = file_get_contents($server . '&apikey=' . $key);
   $hasil = json_decode($data, true);
-
   if($hasil['status'] == 'Gagal') { echo "<h1>Maaf, terjadi kesalahan..</h1>";}
-
   foreach ($hasil['top_skor'] as $key => $value) {
  	?>
 
  	<tr>
  		<th scope="row"><?php echo $value['posisi']; ?></th>
  		<td><?php echo $value['pemain']; ?></td>
- 		<th scope="row"><?php echo $value['klub']; ?></td>
+ 		<th scope="row"><?php echo $value['klub']; ?></th>
  		<td><?php echo $value['gol']; ?></td>
  	</tr>
 
- <?php
-  }
-}
-  ?>
-  <script>$('#komp').text('<?php echo $hasil['kompetisi']; ?>');</script>
+<?php
+  	}
+	}
+?>
+<script>$('#komp').text('<?php echo $hasil['kompetisi']; ?>');</script>
